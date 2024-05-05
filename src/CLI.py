fc45c8b523ec197e4f59107ffe35ec8deb617e06
poyu39 <bruce920309@gmail.com>
2024-05-05 16:43:31 +0800

    feat: 優化指令格式 & 新增 maxterms 和 dontcare

--- src/CLI.py
+++ src/CLI.py
@@ -4,11 +4,15 @@ from TM import TM
 if __name__ == '__main__':
     # example: python CLI.py -v a b c d -m 0 1 2 3 4 6 7 11 12 15
     parser = argparse.ArgumentParser(description='Boolean Algebra Simplifier use Tabulation Method by poyu39')
-    parser.add_argument('--variables', '-v', nargs='+', type=str, help='variables')
-    parser.add_argument('--minterms', '-m', nargs='+', type=int, help='minterms')
+    parser.add_argument('-v', nargs='+', type=str, help='variables')
+    parser.add_argument('-m', nargs='+', type=int, help='minterms')
+    parser.add_argument('-M', nargs='+', type=int, help='maxterms')
+    parser.add_argument('-d', nargs='+', type=int, help='dontcare')
     args = parser.parse_args()
-    variables = args.variables
-    minterms = args.minterms
+    variables = args.v
+    minterms = args.m
+    maxterms = args.M
+    dontcare = args.d
     
     tm = TM(len(variables), minterms)
     minterms_bin = tm.dec2bin()
